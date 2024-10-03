c103f9e1a21a21e1f8a18a9378d374604254e636
haarish2018 <haarishss2018@gmail.com>
2024-10-03 22:59:52 +0530

    -testing

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
-from app.main import app
+# from app.main import app
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
-# lets check deployment
+# # lets check deployment
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    return "Welcome All"
+
+@app.route('/')
+def Error():
+    return "Error on my side"
