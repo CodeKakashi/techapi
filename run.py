092e0621add3e4f5f445731b07e13d99a517190b
haarish2018 <haarishss2018@gmail.com>
2024-10-03 23:21:31 +0530

    -redeploy

--- run.py
+++ run.py
@@ -1,20 +1,4 @@
-# from app.main import app
+from app.main import app
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-# # lets check deployment
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/home')
-def home():
-    return "Welcome All"
-
-@app.route('/')
-def Error():
-    return "Error on my side"
-
-# app.run(port=5008,debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
